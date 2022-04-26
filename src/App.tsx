fa32f21637f161691ce527c5d80dbd28d9ea2307
Daniel F <dfproffesional@gmail.com>
2022-04-26 13:04:47 -0400

    Design Layout header

--- src/App.tsx
+++ src/App.tsx
@@ -6,35 +6,15 @@ import './App.css';
 import Layout from "./components/template/Layout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav>
-          <Link to="/">Home</Link>
-        </nav>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <main>
-        <Layout
-          h1={<h1>Hi</h1>}
-        />
-         
-        <Routes>
-          {/* <Route path="/" element={<Layout />} /> */}
-        </Routes>
-      </main>
-
-    </div>
+    <main>
+      <Layout
+        h1={<h1>Hi</h1>}
+      />
+        
+      <Routes>
+        {/* <Route path="/" element={<Layout />} /> */}
+      </Routes>
+    </main>
   );
 }
 
